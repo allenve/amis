215382d2ca1602a3f67a7c762d15f56368cc0248
nwind <wuduoyi@baidu.com>
2021-04-10 02:41:15 +0000

    Deploying to gh-pages from  @ 3a253b0e48f262b2170a88805a95ae8ea5e14868 🚀

--- pkg/examples/mobile.html_map_1028af5.js
+++ pkg/examples/mobile.html_map_1028af5.js
@@ -9778,7 +9778,7 @@ amis.require.resourceMap({
       "type": "js"
     },
     "p7": {
-      "url": "/amis/pkg/app_847f9e9.js",
+      "url": "/amis/pkg/app_58a69a9.js",
       "type": "js"
     },
     "p1": {
